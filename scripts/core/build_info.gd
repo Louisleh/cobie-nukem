07c7dd06e32e6229612bbdcab4efaf6e3f065851
louisleh <louisjlehmann@gmail.com>
2026-07-11 10:11:39 -0700

    chore: stamp illustrated portrait build

--- scripts/core/build_info.gd
+++ scripts/core/build_info.gd
@@ -2,7 +2,7 @@ class_name BuildInfo
 extends RefCounted
 
 const VERSION := "0.3.0-dev"
-const REVISION := "f7631fb"
+const REVISION := "98b4824"
 const BUILD_ID := "2026-07-11-production-foundation"
 
 static func label() -> String:
