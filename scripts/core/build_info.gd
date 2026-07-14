b566dce3623d261607369386943ce60026365279
louisleh <louisjlehmann@gmail.com>
2026-07-14 10:31:28 -0700

    Restamp Alpha 8 after audio lifecycle fix

--- scripts/core/build_info.gd
+++ scripts/core/build_info.gd
@@ -2,7 +2,7 @@ class_name BuildInfo
 extends RefCounted
 
 const VERSION := "0.6.0-alpha.8"
-const REVISION := "f1f1e53"
+const REVISION := "06fa2d1"
 const BUILD_ID := "2026-07-13-rain-city-forge"
 
 static func label() -> String:
