6228ecd671eaec7bf4eb8e03cb809cef3a82494b
louisleh <louisjlehmann@gmail.com>
2026-07-16 23:06:23 -0700

    Stamp Rain City 0.7.0-alpha.1-rc2

--- scripts/core/build_info.gd
+++ scripts/core/build_info.gd
@@ -1,9 +1,9 @@
 class_name BuildInfo
 extends RefCounted
 
-const VERSION := "0.7.0-alpha.1-rc1"
-const REVISION := "3144a22"
-const BUILD_ID := "2026-07-16-rain-city-rc1"
+const VERSION := "0.7.0-alpha.1-rc2"
+const REVISION := "0d80348"
+const BUILD_ID := "2026-07-16-rain-city-rc2"
 
 static func label() -> String:
 	return "v%s • %s • %s" % [VERSION, REVISION, BUILD_ID]
