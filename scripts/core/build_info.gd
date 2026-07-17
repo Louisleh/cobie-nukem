aaceaf714b114e8bab2bd77eb644a9f924de0c95
louisleh <louisjlehmann@gmail.com>
2026-07-17 13:24:44 -0700

    Stamp 0.7.0 alpha 1 rc3 candidate

--- scripts/core/build_info.gd
+++ scripts/core/build_info.gd
@@ -1,9 +1,9 @@
 class_name BuildInfo
 extends RefCounted
 
-const VERSION := "0.7.0-alpha.1-rc2"
-const REVISION := "0d80348"
-const BUILD_ID := "2026-07-16-rain-city-rc2"
+const VERSION := "0.7.0-alpha.1-rc3"
+const REVISION := "f6ebabd"
+const BUILD_ID := "2026-07-17-startup-stability-rc3"
 
 static func label() -> String:
 	return "v%s • %s • %s" % [VERSION, REVISION, BUILD_ID]
