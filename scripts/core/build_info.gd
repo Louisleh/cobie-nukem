3d46c465a1389d31129544179b57c050db414375
louisleh <louisjlehmann@gmail.com>
2026-07-16 21:22:50 -0700

    release: restamp Rain City rc1 feature revision

--- scripts/core/build_info.gd
+++ scripts/core/build_info.gd
@@ -2,7 +2,7 @@ class_name BuildInfo
 extends RefCounted
 
 const VERSION := "0.7.0-alpha.1-rc1"
-const REVISION := "4888c11"
+const REVISION := "3144a22"
 const BUILD_ID := "2026-07-16-rain-city-rc1"
 
 static func label() -> String:
