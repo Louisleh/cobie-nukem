e07c5cacf83bcd1df1cae061f6cc45b2c1d06694
louisleh <louisjlehmann@gmail.com>
2026-07-17 14:39:27 -0700

    Stamp Rain City selector RC4

--- scripts/core/build_info.gd
+++ scripts/core/build_info.gd
@@ -1,9 +1,9 @@
 class_name BuildInfo
 extends RefCounted
 
-const VERSION := "0.7.0-alpha.1-rc3"
-const REVISION := "ba7c449"
-const BUILD_ID := "2026-07-17-startup-stability-rc3"
+const VERSION := "0.7.0-alpha.1-rc4"
+const REVISION := "e1afd5a"
+const BUILD_ID := "2026-07-17-selector-public-beta-rc4"
 
 static func label() -> String:
 	return "v%s • %s • %s" % [VERSION, REVISION, BUILD_ID]
